b56f5619aaf36f586b12fa7d96476bbdb2efc65f
OnlyCautious <Samy.abbas.of@outlook.com>
2026-07-28 15:50:15 +0200

    Initial Commit

---
+++ DONE/DONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  DONEApp.swift
+//  DONE
+//
+//  Created by Samy Abbas on 28/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct DONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
